c408dbf49c5948b0476ee3662eb47008cb7589bb
CatLogic <yixianxueqi@163.com>
2017-05-22 15:46:39 +0800

    fix directory and travisCI

--- YHDevelopFramework_Swift/Pods/Target Support Files/Pods-YHDevelopFramework_Swift/Pods-YHDevelopFramework_Swift-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_YHDevelopFramework_SwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_YHDevelopFramework_SwiftVersionString[];
-
--- YHDevelopFramework_Swift/Pods/Target Support Files/XCGLogger/XCGLogger-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "XCGLogger.h"
-
-FOUNDATION_EXPORT double XCGLoggerVersionNumber;
-FOUNDATION_EXPORT const unsigned char XCGLoggerVersionString[];
-
